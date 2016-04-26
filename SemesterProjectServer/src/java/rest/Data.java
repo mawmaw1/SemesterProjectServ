a5dfc914be547b62b23e1f8a3ab644f8b4a9c071
mawmaw1 <kontakt@magnuslarsen.dk>
2016-04-26 08:49:29 +0200

    First commit

---
+++ SemesterProjectServer/src/java/rest/Data.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import services.AirlineConnector;
+
+/**
+ * REST Web Service
+ *
+ * @author Magnus
+ */
+@Path("data")
+public class Data {
+
+    AirlineConnector ac = new AirlineConnector();
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of Data
+     */
+    public Data() {
+    }
+
+    /**
+     * Retrieves representation of an instance of rest.Data
+     *
+     * @param airport
+     * @param date
+     * @param persons
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{airport}/{date}/{persons}")
+    public String getAllFlightsFromDate(
+            @PathParam("airport") String airport,
+            @PathParam("date") String date,
+            @PathParam("persons") int persons) throws InterruptedException, ExecutionException, IOException {
+
+        JsonArray result = new JsonArray();
+        List<Future<String>> list = ac.ConnectToAirlines(airport, date, persons);
+       
+        for (Future<String> list1 : list) {
+            
+            JsonObject jsonObject = (new JsonParser()).parse(list1.get()).getAsJsonObject();
+            result.add(jsonObject);
+            
+        }
+        
+        
+        return gson.toJson(result);
+    }
+
+    /**
+     * PUT method for updating or creating an instance of Data
+     *
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/json")
+    public void putJson(String content) {
+    }
+}
---
+++ SemesterProjectServer/src/java/test/Tester.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import services.AirlineConnector;
+
+/**
+ *
+ * @author Magnus
+ */
+public class Tester {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        AirlineConnector ac = new AirlineConnector();
+        List<Future<String>> list = ac.ConnectToAirlines("CPH", "2016-04-25T19:00:00.000Z", 2);
+
+        for (Future<String> list1 : list) {
+
+//            System.out.println(list1.get());
+            JsonObject jsonObject = (new JsonParser()).parse(list1.get()).getAsJsonObject();
+            System.out.println(jsonObject);
+            System.out.println("NEW");
+        }
+
+    }
+
+}
